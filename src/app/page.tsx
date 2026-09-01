006cc69aab350b38b64f279b54a946413574ca9d
Devsinn Technologies <81500860+devsinntechnologies@users.noreply.github.com>
2026-09-01 13:24:11 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import TopDeals from "@/components/home/TopDeals";
 import Hero from "@/components/home/Hero";
 import CulinarySecrets from "@/components/home/CulinarySecrets";
 // import PromoBanners from "@/components/home/PromoBanners";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#fbf7f2] relative overflow-x-hidden">
